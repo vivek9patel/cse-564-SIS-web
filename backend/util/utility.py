b55ae6eaf23ac5763167a6c59c616400cc969b4c
rraisaar <rraisaar@asu.edu>
2023-11-08 15:38:18 -0700

    Update daily moisture irrigation

--- backend/util/utility.py
+++ backend/util/utility.py
@@ -28,4 +28,40 @@ def calculate_watering_frequency(soil_volume, et_rate, precipitation, irrigation
     else:
         num_irrigations_needed = math.ceil(total_water_loss / irrigation_amount)
         days_between_irrigations = days_in_month / num_irrigations_needed
-        return num_irrigations_needed, days_between_irrigations
+        return num_irrigations_needed, float("{:.3f}".format(days_between_irrigations))
+
+def load_irrigation_data(plant_id):
+    try:
+        with open('database/irrigationTracker.json') as f:
+            irrigation_data = json.load(f)
+            return irrigation_data.get(str(plant_id))
+    except FileNotFoundError:
+        abort(404, description="Database file not found")
+    except json.JSONDecodeError:
+        abort(500, description="Error decoding JSON data from file")
+
+
+def getCurrentSoilMoisture(id):
+    irrigation_data = load_irrigation_data(id)
+    print(irrigation_data)
+    return irrigation_data["daily_moisture_level"]
+
+def update_last_irrigation_date(plant_id, new_date):
+    # Specify the path to your JSON file
+    file_path = 'database/irrigationTracker.json'
+    
+    # Read the existing data
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    # Check if the specific plant_id is in the data
+    if str(plant_id) in data:
+        # Update the last_irrigation_date field
+        data[str(plant_id)]['last_irrigation_date'] = new_date
+
+        # Write the data back to the file
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)  # Using indent for pretty-printing
+        return True
+    else:
+        return False
